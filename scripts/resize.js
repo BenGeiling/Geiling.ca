f587624a27137ed41ea3f7502dec1a46902aedcc
BenGeiling <ben@geiling.ca>
2015-11-13 00:16:50 -0500

    v1.0 of my personal website, still needs color/UI tweaks

---
+++ scripts/resize.js
@@ -0,0 +1,21 @@
+//Global variables
+var BODY_HEIGHT;
+var BODY_WIDTH;
+var SCALE;
+
+//Setup calls to scale everything to window size
+window.onload = function() {getScale();}
+$(window).resize(function() {getScale();});
+
+//Get the scale factor and then call pageLoad
+function getScale() {
+	//Get height and width of screen
+	BODY_HEIGHT = $("#background").height();
+	BODY_WIDTH = $("#background").width();
+
+	//Set SCALE factor to the smaller of the two
+	if (BODY_HEIGHT > BODY_WIDTH) {SCALE = BODY_WIDTH;} else {SCALE = BODY_HEIGHT;}
+
+	//Call pageLoad function (must be included somewhere else)
+	pageLoad();
+}
